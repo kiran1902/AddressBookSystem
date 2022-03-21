40cab9346474d7252d2df028da07c2ed0dda5fd9
kiran1902 <kiru2kumar@gmail.com>
2022-03-21 22:05:03 +0530

    Creating the contact details

--- AddressBookSys/AddressBookSys/Program.cs
+++ AddressBookSys/AddressBookSys/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome To Address Book!");
+﻿using System;
+
+namespace AddressBookSys
+{
+    public class Names
+    {
+        static void Main(string[] args) 
+        {
+            string firstname ;
+            string lastname;
+            string address;
+            string city;
+            string state;
+            string zipcode;
+            string email;
+            string phoneNumber;
+
+            Console.WriteLine("Enter the FirstName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the lastName : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the address : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the city : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the state : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the zipcode : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the email : ");
+            Console.ReadLine();
+            Console.WriteLine("Enter the phoneNumber : ");
+            Console.ReadLine();
+
+   
+        }
+     
+        
+    }
+}
+
+
